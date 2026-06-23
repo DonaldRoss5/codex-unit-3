62b6441dc1215f17b541eba58ea2c4b39a92db06
dwayne ross <donaldwross24@gmail.com>
2026-06-23 11:40:19 -0500

    level 5 completed

--- lesson-01-nodejs/level-05-debug-terminal/script.js
+++ lesson-01-nodejs/level-05-debug-terminal/script.js
@@ -1,2 +1,5 @@
 console.log("Roses are red.");
 console.log("Violets are blue.");
+console.log(
+  "1. Set breakpoint.\n2. Choose js Debug Terminal from dropdown.\n3. Run script.\n4. Use debugger control to run each line of code",
+);
