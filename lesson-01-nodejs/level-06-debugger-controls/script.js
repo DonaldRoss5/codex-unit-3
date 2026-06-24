5246d52f69de5d8fa4566c5803533a25e71b49e2
dwayne ross <donaldwross24@gmail.com>
2026-06-23 22:44:17 -0500

    completed lev 6

--- lesson-01-nodejs/level-06-debugger-controls/script.js
+++ lesson-01-nodejs/level-06-debugger-controls/script.js
@@ -1,1 +1,9 @@
-console.log("Attempting to access browser features...");
+try {
+  console.log("Attempting to access browser features...");
+  localStorage.setItem("Monroe", "Louisiana");
+  const pTag = document.querySelector("pTag");
+} catch (error) {
+  console.log(
+    "To access. debugger click on dropp down, select select java script debug terminal",
+  );
+}
