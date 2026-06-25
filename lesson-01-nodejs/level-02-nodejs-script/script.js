c7a8f2b6c9511baf7b3c30f99ae22816aacb7e80
dwayne ross <donaldwross24@gmail.com>
2026-06-24 23:11:40 -0500

    added console.log to lev 2

--- lesson-01-nodejs/level-02-nodejs-script/script.js
+++ lesson-01-nodejs/level-02-nodejs-script/script.js
@@ -1,3 +1,6 @@
 console.log(
   "To run the JS files in the terminal, complete the task then type node + file.",
 );
+console.log(
+  "use the cd command to navigate to specific folder and and node to run the file ",
+);
