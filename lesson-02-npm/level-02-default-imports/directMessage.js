024b2a34fd50f987e464df1e81d7ba23455fc1ee
dwayne ross <donaldwross24@gmail.com>
2026-06-25 19:39:34 -0500

    completed lev 2

--- lesson-02-npm/level-02-default-imports/directMessage.js
+++ lesson-02-npm/level-02-default-imports/directMessage.js
@@ -1,1 +1,2 @@
-export default "";
+export default "Use import,any variable name, the from keyword and the relative path of a file that has a default export";
+
---
+++ lesson-02-npm/level-02-default-imports/index.js
@@ -0,0 +1,5 @@
+import myDefinedMessage from "./definedMessage.js";
+import myDirectMessage from "./directMessage.js";
+
+console.log(myDefinedMessage);
+console.log(myDirectMessage);
