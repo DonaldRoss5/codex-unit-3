27fbf514c45ab2a2f135600bd4c7529aa3f9137c
dwayne ross <donaldwross24@gmail.com>
2026-06-25 19:38:39 -0500

    completed lev 2

--- lesson-02-npm/level-02-default-imports/definedMessage.js
+++ lesson-02-npm/level-02-default-imports/definedMessage.js
@@ -1,1 +1,4 @@
-const definedMessage = "";
+const definedMessage = "Define a value. Save it in a variable. Use export default and athe variable name to. mkae it accessible to other files"
+
+
+export default definedMessage;
