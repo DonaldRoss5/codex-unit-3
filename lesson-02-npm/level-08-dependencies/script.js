5d2089496ae281d22b1146c197a3599007b19d2c
dwayne ross <donaldwross24@gmail.com>
2026-06-27 16:14:05 -0500

    completed lev 8

--- lesson-02-npm/level-08-dependencies/script.js
+++ lesson-02-npm/level-08-dependencies/script.js
@@ -1,1 +1,7 @@
+import chalk from "chalk";
 
+console.log(
+  chalk.cyan(
+    "Dependencies in package.json are packages a project is need to run; the projects imports dependencies and uses them by setting type to module",
+  ),
+);
