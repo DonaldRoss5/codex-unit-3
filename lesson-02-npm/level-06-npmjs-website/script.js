d2a607173977df9f9faa2aafaa1198aa1d8f6d82
dwayne ross <donaldwross24@gmail.com>
2026-06-26 19:22:16 -0500

    completed lev 6

---
+++ lesson-02-npm/level-06-npmjs-website/script.js
@@ -0,0 +1,6 @@
+const messages = [
+  "NPMJS website: is a coding sharing source used to build js",
+  "interesting package found and published: I found cat me it was published 10 years ago ",
+];
+
+console.log(messages);
