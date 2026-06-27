ff22261a0bbdfe5ae5b804751a8a27cfda257de5
dwayne ross <donaldwross24@gmail.com>
2026-06-27 15:56:26 -0500

    completed lev 7

---
+++ lesson-02-npm/level-07-npm-packages/script.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.green("The node modules folder contains code from external modules. Your project imports modules by package name from node_modules. you should: npm install: project import chalk from 'project from project"));
+
+
