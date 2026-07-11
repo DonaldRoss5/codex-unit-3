942bccd55647536aab19fc2a91111d7bc0484afd
dwayne ross <donaldwross24@gmail.com>
2026-07-11 17:48:50 -0500

    completed level 5

---
+++ lesson-04-components/level-05-section-components/react-sections/src/components/Main.jsx
@@ -0,0 +1,12 @@
+export function Main() {
+  return (
+    <main>
+      <p>
+        To convert sections into React components: For the header section, let
+        the Header Function return a the header tag. For the main section, let
+        the Main function return the main tag. For the footer section let a
+        function return the footer tag.
+      </p>
+    </main>
+  );
+}
