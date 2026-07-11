bc33191636334f1b2ab0852375ed1d0bedc82b42
dwayne ross <donaldwross24@gmail.com>
2026-07-11 15:29:07 -0500

    level 07 completed

--- lesson-03-react/level-07-react-template/react-template/src/App.jsx
+++ lesson-03-react/level-07-react-template/react-template/src/App.jsx
@@ -4,17 +4,16 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 
-function App() 
-{
-
+function App() {
   return (
-<main>
-<h1>React Template </h1>
-<p>How to create a React Template</p>
-</main>
-
-
-  )
+    <main>
+      <h1>React Template </h1>
+      <p>
+        To create a React Template, scaffold a React JavaScript project with
+        vite; delete the contents of the App function
+      </p>
+    </main>
+  );
 }
 
 export default App;
