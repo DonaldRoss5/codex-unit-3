b60e99a1817f29eb4bb491495e6a68a4da823d4d
dwayne ross <donaldwross24@gmail.com>
2026-07-14 17:39:31 -0500

    completed lesson 7

---
+++ lesson-04-components/level-07-semantic-template/semantic-template/src/App.jsx
@@ -0,0 +1,18 @@
+import Main from "./components/Main";
+import Header from "./components/Header";
+
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
