d3d9049efd1ebae021ab528d653623fea201d593
dwayne ross <donaldwross24@gmail.com>
2026-07-14 17:45:25 -0500

    completed level 4

---
+++ lesson-04-components/level-04-react-fragment/react-fragment/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>React Fragment</h1>
+      </header>
+      <main>
+        <p>Explain React Fragments: </p>
+      </main>
+      <footer>Website by Donald Ross</footer>
+    </>
+  );
+}
+
+export default App;
