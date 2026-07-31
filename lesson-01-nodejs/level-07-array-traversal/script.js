23a8b763e44f3a2b6ec543e57dc60e7c6a8b7684
dwayne ross <donaldwross24@gmail.com>
2026-07-31 11:21:36 -0500

    completed level 7

--- lesson-01-nodejs/level-07-array-traversal/script.js
+++ lesson-01-nodejs/level-07-array-traversal/script.js
@@ -16,3 +16,17 @@ for (let index = 0; index < animals.length; index++) {
   const animalNumber = index + 1;
   console.log(`${animalNumber}. ${myAnimals}`);
 }
+
+// const cars = [
+// "camry",
+// "mercededs",
+// "land rover",
+// ];
+
+// for (let index = 0; index < cars.length; index++) {
+//   const myAnimals = cars[index];
+//   const myCars = index + 1;
+//   console.log(`${carNumber}. ${myCars}`);
+
+
+// }
