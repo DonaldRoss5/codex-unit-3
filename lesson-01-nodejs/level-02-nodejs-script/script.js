7723fc5aaa18c4d74106b5c32b128cbc034ba5c1
dwayne ross <donaldwross24@gmail.com>
2026-07-31 10:07:50 -0500

    Completed level 2

--- lesson-01-nodejs/level-02-nodejs-script/script.js
+++ lesson-01-nodejs/level-02-nodejs-script/script.js
@@ -2,5 +2,5 @@ console.log(
   "To run the JS files in the terminal, complete the task then type node + file.",
 );
 console.log(
-  "use the cd command to navigate to specific folder and and node to run the file ",
+  "use the cd command to navigate to specific folder and node to run the file ",
 );
