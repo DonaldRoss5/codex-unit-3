649615ade31edd231eebadec3b4e1acdd05b196a
dwayne ross <donaldwross24@gmail.com>
2026-08-01 13:40:34 -0500

    Completed level 8

---
+++ lesson-03-react/level-08-react-page/react-page/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Jordans</h1>
+      <p>
+       Retro 11 Jordans are great basketball shoes
+      </p>
+      <img src="https://www.nike.com/" alt="Nike"/> 
+      <img src="https://www.jdsports.com/" alt="JD Sports"/> 
+      <img src="https://www.finishline.com/" alt="Finish Line"/>
+    </main>
+  );
+}
+
+export default App;
