b688b6eb2dc7407e41d488c1c966407ec27d71d7
dwayne ross <donaldwross24@gmail.com>
2026-08-03 20:30:10 -0500

    Completed level 1

--- lesson-04-components/level-01-global-packages/script .js
+++ lesson-04-components/level-01-global-packages/script .js
@@ -1,5 +1,5 @@
 console.log(
-  "Global packages can be installed with this command: npm install --globall pnpm the-name-of-the-package; or use the shortcut or you can use the shortcut key: -g npm i ",
+  "Global packages can be installed with this command: npm install --global pnpm the-name-of-the-package; or use the shortcut key: -g or npm i or npm i -g ",
 );
 
 // NOTES: INSTALL PACKAGES AND RUN IN TIRMINAL
