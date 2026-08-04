7a30857797641fa9bf96ba93d8c9b3851c444f88
dwayne ross <donaldwross24@gmail.com>
2026-08-04 17:47:15 -0500

    Completed level 7

--- lesson-04-components/level-07-semantic-template/semantic-template/src/components/Main.jsx
+++ lesson-04-components/level-07-semantic-template/semantic-template/src/components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
       <p>
         To make semati template create a body tag that has semantic HTML:
         header, main, and footer. A script tag should have the defer attribute
-        when in the head tag
+        when in the head tag.
       </p>
     </main>
   );
