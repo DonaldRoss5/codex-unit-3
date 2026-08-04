913e3905aa68d7dc79706587b6a28307777d865a
dwayne ross <donaldwross24@gmail.com>
2026-08-04 11:05:39 -0500

    Completed level 6

--- lesson-04-components/level-06-react-root/react-root/src/main.jsx
+++ lesson-04-components/level-06-react-root/react-root/src/main.jsx
@@ -8,6 +8,11 @@ const root = createRoot(divTag);
 root.render(
   <main>
     <h1>React Root</h1>
-    <p>Explain React Root</p>
+    <p>
+      React renders HTML into a root element. From index.html the root element
+      is selected in main.jsx by getElementById. createRoot uses the root
+      element in which HTML will be rendered. The Root object uses a render
+      method and renders HTML into the root element.
+    </p>
   </main>,
 );
