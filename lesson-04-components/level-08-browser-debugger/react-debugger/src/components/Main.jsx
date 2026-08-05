7ad037c025a707dbf75df4575f2236afe9d2c30d
dwayne ross <donaldwross24@gmail.com>
2026-08-04 19:46:06 -0500

    Completed level 8

--- lesson-04-components/level-08-browser-debugger/react-debugger/src/components/Main.jsx
+++ lesson-04-components/level-08-browser-debugger/react-debugger/src/components/Main.jsx
@@ -1,6 +1,6 @@
 export default function Main() {
   const message =
-    "Start vite server, open the Run and Debug panel, select React Debugger from the dropdown and press play";
+    "the debugger must first be manually configured by editing the settings file in JSON format, so its content is just like an object, each property is in quotations. must be manually configured by editing the settings file.The settings file is in JSON format, so its content is just like an object, except that each property is in quotations. Open the Run and Debug panel, select React Debugger from the dropdown and press play";
 
   return (
     <main>
