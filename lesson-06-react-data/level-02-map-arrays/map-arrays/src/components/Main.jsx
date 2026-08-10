b35ac4d2fb94cf5dd0bc8ab1f9a720c77cbeada6
dwayne ross <donaldwross24@gmail.com>
2026-08-10 18:40:02 -0500

    Completed level 2

--- lesson-06-react-data/level-02-map-arrays/map-arrays/src/components/Main.jsx
+++ lesson-06-react-data/level-02-map-arrays/map-arrays/src/components/Main.jsx
@@ -1,7 +1,59 @@
-export default function Main (){
-return (
+import { useEffect, useState } from "react";
+import { data } from "../data/data";
 
-    <main><p>Explain how to make semati template</p></main>
-)
+export function Main() {
+    debugger;
+  const [didMount, setDidMount] = useState(false);
+  const [subjects, setSubjects] = useState([]);
+  useEffect(componentDidMount, []);
 
+  return (
+    <main>
+      <p>{"didMount: " + didMount}</p>
+      <section>{subjects}</section>
+
+      <p>Explain how to make semati template</p>
+    </main>
+  );
+
+  function componentDidMount() {
+    debugger;
+    setDidMount(true);
+    const details = data.map(toDetails);
+    setSubjects(details);
+  }
+
+  function toDetails(dataItem) {
+    const details = (
+    <details>
+<summary>{dataItem.name}</summary>
+{dataItem.description}
+
+    </details>
+    );
+    return details
+  }
 }
+
+export default Main;
+
+
+
+
+
+
+
+ // const detailsElements = [];
+    // for (let index = 0; index < data.length; index++){
+    // debugger;
+    // const item = data[index];
+    // const details = (
+    // <details>
+    //     <summary>{item.name}</summary>
+    //     {item.description}
+    // </details>
+    // );
+    // detailsElements.push(details);
+    //   }
+    //   setSubjects(detailsElements);
+    // }
