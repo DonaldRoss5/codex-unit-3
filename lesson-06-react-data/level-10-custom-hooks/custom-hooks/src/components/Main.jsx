977ad9eecd6c73fa40bad462cc844df2d2d1bf2a
dwayne ross <donaldwross24@gmail.com>
2026-08-15 13:03:14 -0500

    Level 10 completed

---
+++ lesson-06-react-data/level-10-custom-hooks/custom-hooks/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import { Slides } from "./Slides";
+
+export function Main (){
+return (
+
+    <main>
+        <p>Explain how to make semati template</p>
+    <Slides />    
+    </main>
+)
+
+}
+
